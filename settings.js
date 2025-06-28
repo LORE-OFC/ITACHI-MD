7bf14305a7fb3b629fc26a81cbbefa50eb74d385
fedelanyt130 <federicoyt150@gmail.com>
2025-06-28 14:55:52 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -59,7 +59,7 @@ global.moneda = '¥enes'
 global.welcom1 = '❍ Edita Con El Comando setwelcome'
 global.welcom2 = '❍ Edita Con El Comando setbye'
 global.banner = 'https://qu.ax/UUgNU.jpg'
-global.avatar = 'https://files.catbox.moe/oly31v.jpeg'
+global.avatar = 'https://qu.ax/loMwz.jpg'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
